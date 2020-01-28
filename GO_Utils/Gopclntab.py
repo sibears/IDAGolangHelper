58865f9e6c167221a81494d79f83b656e56fd824
Stephan Simon <stephan.@firehak.com>
2020-01-28 15:31:58 -0500

    Remove leftover debugging
    Removed some debugging I accidentally left in the previous commit

--- GO_Utils/Gopclntab.py
+++ GO_Utils/Gopclntab.py
@@ -29,10 +29,6 @@ def check_is_gopclntab(addr):
 def findGoPcLn():
     end_ea = idc.get_segm_end(0)
     possible_loc = ida_search.find_binary(0, end_ea, lookup, 16, idc.SEARCH_DOWN) #header of gopclntab
-    print("Possible gopclntab: %s" % hex(possible_loc))
-
-    # gopclntab = ida_segment.get_segm_by_name('.gopclntab')
-    # print("Possible gopclntab: %s" % hex(gopclntab))
 
     if check_is_gopclntab(possible_loc):
         return possible_loc
